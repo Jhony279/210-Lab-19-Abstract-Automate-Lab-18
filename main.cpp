244dcfab384ed43fc6aebd78cc1a8973063ceb30
Jhony279 <lordjhony732@gmail.comm>
2026-03-08 18:43:33 -0700

    Created class and struct to hold movies and their reviews and rstings

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 // COMSC-210 | Lab 19 | Johnathan Perez Baltazar
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// Function Prototypes:
+struct Node{
+    int rating;
+    string reviewComment;
+    Node* next;
+};
+
+class Movie{
+    string title;
+    Node *listHead;
+};
 
 /**
  * @brief - Description of main
  * @return 
 */
 int main() {
+
+
     return 0;
 }
-
-// Function Definitions:
-
-// Reminder lines must be 80 characters or less, no more than one empty line, no magic numbers,
-// and Reminder to comment code where neccessary.
